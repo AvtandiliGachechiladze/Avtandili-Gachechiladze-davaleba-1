f7cd4faaa66ed07d80e468dffc6d24add324e4e6
AvtandilG <agachechiladze@crocobet.com>
2021-05-19 20:09:39 +0400

    upload of repository

---
+++ src/main/java/ge/edu/btu/Avtandili_Gachechiladze_davaleba_1/Avtandili_Gachechiladze_weather/HelloApplication.java
@@ -0,0 +1,8 @@
+package ge.edu.btu.Avtandili_Gachechiladze_davaleba_1.Avtandili_Gachechiladze_weather;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+}
